c205e34ad5f85fd81a70a7160d0ef770ba975a3b
Felipe Souza <fells778@gmail.com>
2023-03-29 21:11:04 -0300

    add dependência/file de constraint

---
+++ app/src/main/java/com/example/jetpackcomposecatalgo/ConstraintExample.kt
@@ -0,0 +1,53 @@
+package com.example.jetpackcomposecatalgo
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+
+@Preview
+@Composable
+fun ConstraintExample01() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+
+        val (boxRed, boxBlue, boxYellow, boxMagenta) = createRefs()
+
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                start.linkTo(parent.start)
+                bottom.linkTo(parent.bottom)
+                end.linkTo(parent.end)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Blue)
+            .constrainAs(boxBlue) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                bottom.linkTo(boxBlue.top)
+                start.linkTo(boxBlue.end)
+            })
+        Box(modifier = Modifier
+            .size(125.dp)
+            .background(Color.Magenta)
+            .constrainAs(boxMagenta) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
+    }
+}
